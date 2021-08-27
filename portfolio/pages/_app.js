38bdda868c8ff701e26ea123628727612f2e63bf
Noctturne <88271478+Noctturne@users.noreply.github.com>
2021-08-27 15:11:22 +0200

    Create next app with bootstrap and sass

---
+++ portfolio/pages/_app.js
@@ -0,0 +1,19 @@
+/* BOOTSTRAP */
+import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/globals.css'
+import '../styles/globals.scss'
+
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp
